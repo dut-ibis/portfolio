c7141599e0ad1dc1ab7081f514de0c8165d081f9
unknown <iw2155@nyu.edu>
2025-02-11 22:36:00 -0500

    moved pages to src

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import { Test } from '../pages/Test';
-import App from '../pages/App';
+import { Test } from './pages/Test';
+import App from './pages/App';
 
 const rootElement = document.getElementById('root');
 

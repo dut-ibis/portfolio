12aaa4df7850af80956d1b0074a78ba93860a32e
unknown <iw2155@nyu.edu>
2025-02-14 00:40:51 -0500

    blalbaa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,6 +20,14 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/,''),
       },
     },
-  },
+  }, 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve('client', 'index.html'), 
+        public: resolve('public', 'public/index.html')
+      }
+    }
+  }, 
   logLevel: 'warn', 
 })
